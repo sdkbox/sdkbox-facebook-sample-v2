9fc25cd22df1289ab329f54bc534b6c2607b82d7
wanghuan <wanghuan1115@live.cn>
2015-11-16 16:36:14 +0800

    sdkbox update for cpp

--- projects/FacebookSample/proj.ios/PluginFacebook.framework/Versions/A/Headers/PluginFacebook.h
+++ projects/FacebookSample/proj.ios/PluginFacebook.framework/Versions/A/Headers/PluginFacebook.h
@@ -12,6 +12,8 @@
 
 namespace sdkbox
 {
+    class Json;
+
     const std::string FB_PERM_READ_PUBLIC_PROFILE("public_profile");
     const std::string FB_PERM_READ_EMAIL("email");
     const std::string FB_PERM_READ_USER_FRIENDS("user_friends");
@@ -19,12 +21,21 @@ namespace sdkbox
     const std::string FB_API_ME_FRIENDS_TAG("__fb_me_friends__");
     const std::string FB_API_ME_FRIENDS_PATH("me/friends");
 
+
+    const std::string kRI_ExcludeFromList("ExcludeFromListIds");
+    const std::string kRI_PictureSize("PictureSize");
+    const std::string kRI_PaginationLimit("PaginationLimit");
+    const std::string kRI_ResponseFields("ResponseFields");
+
+
     enum FBShareType
     {
         FB_NONE  = 0,
         FB_LINK  = 1,
         FB_PHOTO = 2
     };
+
+    typedef std::map<std::string, std::string> FBAPIParam;
 
     struct FBShareInfo
     {
@@ -40,15 +51,159 @@ namespace sdkbox
         std::string image;
     };
 
-    struct FBGraphUser
+    class FBGraphUser
     {
-        FBGraphUser();
 
+    public:
+
+        // konstants for Graph User
+        static const std::string kGU_USERID;
+        static const std::string kGU_NAME;
+        static const std::string kGU_FIRST_NAME;
+        static const std::string kGU_LAST_NAME;
+        static const std::string kGU_EMAIL;
+        static const std::string kGU_PICTURE_URL;
+        static const std::string kGU_PICTURE_IS_SILHOUETTE;
+        static const std::string kGU_SCORE;
+        static const std::string kGU_INSTALLED;
+
+        static const std::string kGU_STR_TRUE;
+        static const std::string kGU_STR_FALSE;
+
+        FBGraphUser();
+        explicit FBGraphUser( const sdkbox::Json& json );
+        explicit FBGraphUser( const std::string& json );
+
+        // FB user id
+        // @deprecated use getUserId()
         std::string uid;
+
+        // FB user name ( normally firstName + lastName )
+        // @deprecated use getUserName()
         std::string name;
+
+        // User first name
+        // @deprecated use getFirstName()
         std::string firstName;
+
+        // User last name
+        // @deprecated use getLastName()
         std::string lastName;
+
+        // @deprecated use isAppInstalled()
         bool        isInstalled;
+
+        std::string getUserId() const;
+        std::string getName() const;
+        std::string getFirstName() const;
+        std::string getLastName() const;
+        std::string getEmail() const;
+        std::string getPictureURL() const;
+        bool        isAppInstalled() const;
+        bool        isPictureSilhouette() const;
+        bool        asBoolean( const std::string& ) const;
+
+        const std::string getField( const std::string& field ) const;
+        void setField( const std::string& field, const std::string& value );
+
+        /**
+         * the bool value will be converted to 'true' (kGU_STR_TRUE) or 'false' (kGU_STR_FALSE)
+         */
+        void setField( const std::string& field, bool value );
+
+        Json toJSON() const;
+        std::string toJSONString() const;
+
+        std::map<std::string, std::string> getFields() const;
+
+    private:
+
+        std::map<std::string, std::string>  _fields;
+
+        void __initFromJSON( const Json& json );
+
+    };
+
+    struct FBInvitableUsersCursor {
+        std::string next_url;
+        std::string prev_url;
+        std::string next_cursor;
+        std::string prev_cursor;
+
+        FBInvitableUsersCursor( const sdkbox::Json& json );
+        FBInvitableUsersCursor( );
+    };
+
+
+    /**
+     * FBInvitableFriendsInfo is returned as a result of a call to the FB plugin's method:
+     * <code>requestInvitableFriends</code>.
+     *
+     * The idea is that with this information you can build a custom invitation dialog for your
+     * FB contacts. Next to the name, there's an invitation token (FBGraphUser).
+     * After the user makes the selection of people to invite, a call to the method
+     * <code>inviteFriendsWithInviteIds</code> with an array of the desired invitation tokens
+     * will invoke a FB dialog with invitation which will eventually send the invitations.
+     *
+     */
+    class FBInvitableFriendsInfo {
+
+    private:
+        std::vector<FBGraphUser>    _invitableUsersInfo;
+        FBInvitableUsersCursor      _cursorInfo;
+
+        // the originally returned string for the FB graph call.
+        std::string                 _originalJSONString;
+
+    public:
+        FBInvitableFriendsInfo( const std::string& json );
+        FBInvitableFriendsInfo* init();
+
+        typedef std::vector<FBGraphUser>::iterator iterator;
+        typedef std::vector<FBGraphUser>::const_iterator const_iterator;
+
+        size_t getNumInvitationTokens() const {
+            return _invitableUsersInfo.size();
+        }
+
+        iterator begin() { return _invitableUsersInfo.begin(); }
+        const_iterator begin() const { return _invitableUsersInfo.begin(); }
+        iterator end() { return _invitableUsersInfo.end(); }
+        const_iterator end() const { return _invitableUsersInfo.end(); }
+
+        const std::string& getOriginalString() const {
+            return _originalJSONString;
+        }
+
+        /**
+         * Request this url to get the next invitable friends document info.
+         * Maybe empty.
+         */
+        const std::string& getNextURL() const {
+            return _cursorInfo.next_url;
+        }
+
+        /**
+         * Request this url to get the prev invitable friends document info.
+         * Maybe empty;
+         */
+        const std::string& getPrevURL() const {
+            return _cursorInfo.prev_url;
+        }
+
+        /**
+         * Use this string to build the NextURL.
+         */
+        const std::string& getNextCursor() const {
+            return _cursorInfo.next_cursor;
+        }
+
+        /**
+         * Use this string to build the PrevURL.
+         */
+        const std::string& getPrevCursor() const {
+            return _cursorInfo.prev_cursor;
+        }
     };
 
     class FacebookListener
@@ -61,12 +216,17 @@ namespace sdkbox
         virtual void onAPI(const std::string& key, const std::string& jsonData) = 0;
         virtual void onPermission(bool isLogin, const std::string& msg) = 0;
         virtual void onFetchFriends(bool ok, const std::string& msg) = 0;
+        virtual void onRequestInvitableFriends( const FBInvitableFriendsInfo& friends ) = 0;
+        virtual void onInviteFriendsWithInviteIdsResult( bool result, const std::string& msg )= 0;
+        virtual void onInviteFriendsResult( bool result, const std::string& msg )= 0;
+
+        virtual void onGetUserInfo( const FBGraphUser& userInfo ) = 0;
     };
 
     class PluginFacebook
     {
+
     public:
-        typedef std::map<std::string, std::string> FBAPIParam;
 
         /*!
          * initialize the plugin instance.
@@ -90,8 +250,12 @@ namespace sdkbox
 
         /**
          * @brief log in
+         *
+         * This method calls login with a single permission: sdkbox::FB_PERM_READ_PUBLIC_PROFILE
          */
         static void login();
+
+        static void login( std::vector<std::string>& permissions );
 
         /**
          * @brief log in with specific read permissions, conflict with publish permissions
@@ -166,6 +330,9 @@ namespace sdkbox
 
         /**
          * @brief fetch friends data from Facebook
+         *
+         * This data only reflects your friends that are using the app.
+         * The number of friends defaults to 25.
          */
         static void fetchFriends();
 
@@ -178,6 +345,30 @@ namespace sdkbox
          * @brief check whether can present Facebook App
          */
         static bool canPresentWithFBApp(const FBShareInfo& info);
+
+        /**
+         * Get a vector of invitable friends info which can be used to build a custom friends invite dialog.
+         *
+         * The default set will be limited to 25 friends.
+         * The order in which FB sorts the friends, and which ones returns vary between calls.
+         * The returned invitation tokens are not supposed to be long-term stored and may vary between
+         * calls for the same friends.
+         *
+         * The application must have a canvas configuration for this API call to work.
+         *
+         */
+        static void requestInvitableFriends( const FBAPIParam& );
+
+        /**
+         * Invite friends based on the result obtained from a call to <code>requestInvitableFriends</code>
+         */
+        static void inviteFriendsWithInviteIds( const std::vector<std::string>& invite_ids, const std::string& title, const std::string& invite_text );
+
+        /**
+         * Use the default FB dialog to invite friends.
+         */
+        static void inviteFriends( const std::string& app_link_url, const std::string& preview_image_url );
+
     };
 }
 
